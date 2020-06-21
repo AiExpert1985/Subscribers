1cb45cb6701e78a2c52979bb6a656c6d8f476c3f
Mohamad Nawfal <hamandi.mohammed@gmail.com>
2020-06-21 12:41:32 +0300

    complete the upload of multiple files, and processing them into the database

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, jsonify, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///sqlite/subscribers.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-UPLOAD_FOLDER = './db'
+UPLOAD_FOLDER = './upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['txt'])
 
@@ -38,6 +39,7 @@ class Subscriber(db.Model):
     record = db.Column(db.String, nullable=True)
     page = db.Column(db.String, nullable=True)
     meem_yaa_at = db.Column(db.String, nullable=True)
+    hissab = db.Column(db.String, nullable=True)
 
     @property
     def serialize(self):
@@ -61,18 +63,18 @@ class Subscriber(db.Model):
             'meem_yaa': self.meem_yaa,
             'record': self.record,
             'page': self.page,
-            'meem_yaa_at': self.meem_yaa_at
+            'meem_yaa_at': self.meem_yaa_at,
+            'hissab': self.hissab
         }
+
 
 def log_error(error):
     errorLog = open("error_log.txt","a")
     errorLog.write(f"{error}\n{datetime.now()}\n \n")
     errorLog.close()
 
-def scanFile():
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+
+def scan_file(file_names):
     layout = {
         "line_one": [("total", "other", "debts", "counter", "breaker", "curr_reading_at", "curr_reading",
                       "prev_reading_at", "prev_reading", "name", "account"),
@@ -83,50 +85,65 @@ def scanFile():
         "title": [("meem_yaa", "record", "page", "meem_yaa_at"),
                   ((39, 45), (90, 94), (108, 114), (116, -1))]
     }
-    try:
-        input_file = open("files/group_1.TXT", encoding="utf-8")
-        lines = input_file.readlines()
-    except:
-        log_error("error while read the subscribers info file")
-    scanDict = {}
-    for newLine in lines:
-        if "جدول القوائم المطبوعة" in newLine:
-            scanDict["title"] = newLine
-            continue
-        if newLine.count('/') >= 2 and len(newLine) > 100:
-            scanDict["line_one"] = newLine
-            continue
-        if scanDict.get("line_one") and (newLine[0] == " " and not newLine.count('*') >= 1):
-            scanDict["line_two"] = newLine
-        if scanDict.get("line_one"):
-            kwQuery = {}
-            for line_name, line_data in scanDict.items():
-                col_names, col_indexes = layout[line_name]
-                for n, indexes in enumerate(col_indexes):
-                    db_col_name = col_names[n]
-                    start = indexes[0]
-                    end = indexes[1]
-                    db_col_data = line_data[start:end]
-                    try:
-                        kwQuery[db_col_name] = db_col_data.strip()
-                    except:
-                        log_error("error while read the inserting subscribers data to the database")
-            if scanDict:
-                db.session.add(Subscriber(**kwQuery))
-            del scanDict["line_one"]
-            if scanDict.get("line_two"):
-                del scanDict["line_two"]
+    # del previous database content, to prepare it for the new files
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+    for file_name in file_names:
+        try:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'],file_name)
+            input_file = open(file_path, encoding="utf-8")
+            lines = input_file.readlines()
+        except:
+            log_error(f"error while read {file_name}")
+        scanDict = {}
+        for newLine in lines:
+            if "جدول القوائم المطبوعة" in newLine:
+                scanDict["title"] = newLine
+                continue
+            if newLine.count('/') >= 2 and len(newLine) > 100:
+                scanDict["line_one"] = newLine
+                continue
+            if scanDict.get("line_one") and (newLine[0] == " " and not newLine.count('*') >= 1):
+                scanDict["line_two"] = newLine
+            if scanDict.get("line_one"):
+                kwQuery = {}
+                for line_name, line_data in scanDict.items():
+                    col_names, col_indexes = layout[line_name]
+                    for n, indexes in enumerate(col_indexes):
+                        db_col_name = col_names[n]
+                        start = indexes[0]
+                        end = indexes[1]
+                        db_col_data = line_data[start:end]
+                        try:
+                            kwQuery[db_col_name] = db_col_data.strip()
+                        except:
+                            log_error("error while read the inserting subscribers data to the database")
+                kwQuery['hissab'] = f"{kwQuery['record']}{kwQuery['account'][2:]}"
+                if scanDict:
+                    db.session.add(Subscriber(**kwQuery))
+                del scanDict["line_one"]
+                if scanDict.get("line_two"):
+                    del scanDict["line_two"]
     db.session.commit()
+
 
 @app.route("/")
 def index():
-    scanFile()
+    # scan_file()
     return render_template("index.html")
+
 
 @app.route("/download", methods=["POST"])
 def download():
-    # file = request.files['file']
+    temp_names = []
+    for temp_file in request.files.getlist("file[]"):
+        temp_file.save(os.path.join(app.config["UPLOAD_FOLDER"], temp_file.filename))
+        temp_names.append(temp_file.filename)
+    scan_file(temp_names)
+    return render_template("index.html", test="done")
     return send_from_directory(app.config['UPLOAD_FOLDER'], "subscribers.accdb", as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
